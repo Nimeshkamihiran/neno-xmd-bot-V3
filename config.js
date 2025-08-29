98cac0e8ecc7d44804459c8f1d1bbc3f73e3738e
NIMESHKA MIHIRAN ❤️💪 <nimeshmihiranga183@gmail.com>
2025-08-30 04:33:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "aMA3ATxL#P8tF1sQwOdQZwgys992W4RbRU5FamAdjfU7eLuxUMMY",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
+    MODE: process.env.MODE || "public",
+    AUTO_VOICE: process.env.AUTO_VOICE || "true",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/nqCsIHZ.jpeg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR DO YOU WANT ME IAM HERE☺️😊♻️",
+    ANTI_LINK: process.env.ANTI_LINK || "true",
+    ANTI_BAD: process.env.ANTI_BAD || "true",
+    PREFIX: process.env.PREFIX || ".",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+    AUTO_REACT: process.env.AUTO_REACT || "true",
+    HEART_REACT: process.env.HEART_REACT || "true",
+    OWNER_REACT: process.env.OWNER_REACT || "true",
+    BOT_NAME: process.env.BOT_NAME || "ɴᴇɴᴏ ᴠ3",
+    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+};
